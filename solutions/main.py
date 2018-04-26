49ecb71d7147e1aafa98cb60aa9dd7be9ac2d7ad
juansca <juansca1229@gmail.com>
2018-04-26 20:12:37 -0300

    Initial commit

---
+++ solutions/main.py
@@ -0,0 +1,12 @@
+from dicegame.runner import GameRunner
+
+
+def main():
+    print("Introduce los valores del dado")
+    print("Es así de fácil")
+    print("Qué es de tu vida")
+    GameRunner.run()
+
+
+if __name__ == "__main__":
+    main()
